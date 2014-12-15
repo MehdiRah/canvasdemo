8df4d967bf3f1b913487301eaa2af8dc1c1de528
Mehdi Rahman <mehdirahman@icloud.com>
2014-12-15 11:23:45 +0000

    reintroduce stylus coz i brokded it

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 var express = require('express'),
-    // stylus = require('stylus'), 
-    // nib = require('nib');
+    stylus = require('stylus'), 
+    nib = require('nib');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
-// function compile(str, path) {
-//   return stylus(str)
-//     .set('filename', path)
-//     .use(nib())
-// }
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib())
+}
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
 // app.use(express.logger('dev'))
-// app.use(stylus.middleware(
-//   { src: __dirname + '/public'
-//   , compile: compile
-//   }
-// ))
+app.use(stylus.middleware(
+  { src: __dirname + '/public'
+  , compile: compile
+  }
+))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function (req, res) {
