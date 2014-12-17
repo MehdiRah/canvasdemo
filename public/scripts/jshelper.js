9d11fd500055fb8fa753ad27098213acabcfb1b9
Mehdi Rahman <mehdirahman@icloud.com>
2014-12-17 16:47:28 +0000

    js changes

--- public/scripts/jshelper.js
+++ public/scripts/jshelper.js
@@ -1,10 +1,9 @@
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-
-<script>
-$(function(){
-	console.log('canvas app init');
-
-});
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 
 
+<script type="text/javascript">
+	$(function(){
+		console.log('canvas app init');
+
+	});
 </script>
