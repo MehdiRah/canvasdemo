23efdf4e1d22cb93796e5f3e0539c962a49623e3
Mehdi Rahman <mehdirahman@icloud.com>
2014-12-17 17:14:01 +0000

    stupid jade syntax

--- public/scripts/jshelper.js
+++ public/scripts/jshelper.js
@@ -1,4 +1,4 @@
-script(type='text/javascript' src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js")
+script(src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js")
 
 script(type='text/javascript').
     $(function(){
