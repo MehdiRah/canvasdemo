d6b52d3c10b9dd260a3ec3ff73a2f8fae88ced86
Mehdi Rahman <mehdirahman@icloud.com>
2014-12-17 21:25:45 +0000

    Feature progression

--- public/scripts/jshelper.js
+++ public/scripts/jshelper.js
@@ -1,3 +1,12 @@
 $(function(){
 	console.log('canvas app init');
+	$('#btngreen').on('click', function(){
+		console.log('Click Green');
+	});
+	$('#btnyellow').on('click', function(){
+		console.log('Click Yellow');
+	});
+	$('#btnred').on('click', function(){
+		console.log('Click Red');
+	});
 });
