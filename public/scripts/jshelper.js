0405e8d3ad7e1100b6f818b1b631c8f0a80fbb24
Mehdi Rahman <mehdirahman@icloud.com>
2014-12-17 16:54:06 +0000

    stupid jade syntax

--- public/scripts/jshelper.js
+++ public/scripts/jshelper.js
@@ -1,6 +1,5 @@
 <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 
-
 <script type="text/javascript">
 	$(function(){
 		console.log('canvas app init');
